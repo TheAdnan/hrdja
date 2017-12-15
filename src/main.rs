b45dfc0f0b84c6a56b2eb6cf54934550c746aac9
TheAdnan <adnankicin92@gmail.com>
2017-12-15 15:36:45 +0100

    tuples and functions

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::io::stdin;
 use rand::thread_rng;
 
 fn main() {
-    println!("Selam alejk my brozer\n");
+
+	let bolt_sign = '🔩';
+
+    println!("{} Selam alejk my brozer {} \n", bolt_sign, bolt_sign);
     
     println!("You can reply here: ");
 
@@ -13,12 +16,19 @@ fn main() {
 
     stdin().read_line(&mut input_str).expect("An error ocurred");
 
-    println!("\nYou replied: '{}'", input_str);
+    println!("\nYou replied: {}", input_str);
 
     generate_secret_number();
+
+    println!("\Random generated tuple: {:?}", generate_tuple_random());
 }
 
 fn generate_secret_number(){
 	let number = thread_rng().gen_range(1,33);
 	println!("Secret number is: {}", number);
+}
+
+fn generate_tuple_random() -> (u32, u32, u32){
+	let tup = (thread_rng().gen_range(1,33), thread_rng().gen_range(1,33), thread_rng().gen_range(1,33));
+	return tup
 }
