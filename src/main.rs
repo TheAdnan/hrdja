2281e9bf23e67b40d6c227ed5e15c38b81cf3481
TheAdnan <adnankicin92@gmail.com>
2017-12-15 15:08:47 +0100

    generate_random_number function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io::stdin;
+use rand::thread_rng;
 
 fn main() {
     println!("Selam alejk my brozer\n");
@@ -10,4 +14,11 @@ fn main() {
     stdin().read_line(&mut input_str).expect("An error ocurred");
 
     println!("\nYou replied: '{}'", input_str);
+
+    generate_secret_number();
+}
+
+fn generate_secret_number(){
+	let number = thread_rng().gen_range(1,33);
+	println!("Secret number is: {}", number);
 }
