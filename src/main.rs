69acf4587f241e7fa4bc870dd1f68a8c7d72f28b
TheAdnan <adnankicin92@gmail.com>
2017-12-15 14:55:06 +0100

    read string 🔩

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Selam alejk my brozer\n");
+    
+    println!("You can reply here: ");
+
+    let mut input_str = String::new();
+
+    stdin().read_line(&mut input_str).expect("An error ocurred");
+
+    println!("\nYou replied: '{}'", input_str);
 }
