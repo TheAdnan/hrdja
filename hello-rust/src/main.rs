d61ee1eaa8bffa821fdd6b56c08aa4b853296530
TheAdnan <adnankicin92@gmail.com>
2018-02-15 13:06:30 +0100

    minesweeper init :crab:

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,46 @@
+extern crate rand;
+use rand::Rng;
+
+#[derive(Clone)]
+struct Mine{
+    position: (u32, u32)
+}
+
+#[derive(Clone)]
+struct MineField{
+    size: (u32, u32),
+    mines: Vec<Mine>
+}
+
+impl MineField{
+    pub fn generate_mines(mut self, num_of_mines: u32) -> MineField{
+        for i in 0..num_of_mines{
+            let mina = Mine{
+                position: (rand::thread_rng().gen_range(self.size.0, self.size.1), rand::thread_rng().gen_range(self.size.0, self.size.1))
+            };
+            self.mines.push(mina);
+        }
+        self
+    }
+
+    pub fn print_field(self){
+        for i in self.mines{
+            println!("{:?}\n", i.position);
+        }
+    }
+}
+
+
+fn main() {
+
+    let mut polje = MineField{
+        size: (22, 44),
+        mines: Vec::new()
+    };
+
+    polje = polje.generate_mines(12);
+
+    polje.print_field();
+
+
+}
